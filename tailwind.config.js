817cb3515275de738d1adb7c09f51319e12a2975
Damir Khalimov <xalimovdamir@gmail.com>
2023-08-30 16:01:25 +0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.tsx'
+  ],
+  theme: {
+    fontFamily: {
+      sans: 'Manrope, sans-serif'
+    },
+    extend: {
+      colors: {
+      },
+      spacing: {
+        13: '3.25rem'
+      }
+    },
+  },
+  plugins: [],
+}
+
